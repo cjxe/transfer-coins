28f603e5acd7111deb27719993275994d3aabd9c
mbaranturkmen <kilexey@gmail.com>
2023-03-11 10:56:27 +0000

    create component `ToggleSwitch`

---
+++ src/components/ToggleSwitch.stories.jsx
@@ -0,0 +1,21 @@
+import ToggleSwitch from './ToggleSwitch';
+
+export default {
+  title: 'Components/ToggleSwitch',
+  component: ToggleSwitch,
+};
+
+export const Base = {
+  args: {
+    id: 'base',
+  },
+};
+
+export const Full = {
+  args: {
+    id: 'full',
+    labelText: 'Dark mode',
+    helperText: "This option is saved in your browser's cookies",
+    // checked: 'checked', // ...props
+  },
+};
