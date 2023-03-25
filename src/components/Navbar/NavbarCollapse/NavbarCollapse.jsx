f35f293d0c4e691ba4d9fad9f61888bd5093b295
mbaranturkmen <kilexey@gmail.com>
2023-03-25 16:22:31 +0000

    fix `Navbar`: xs:increase margin to fix the letter bug

--- src/components/Navbar/NavbarCollapse/NavbarCollapse.jsx
+++ src/components/Navbar/NavbarCollapse/NavbarCollapse.jsx
@@ -9,7 +9,7 @@ function NavbarCollapse({ children }) {
     <div
       className={classNames(
         'h-full grow transition-opacity duration-300 ease-in xs:mt-8 xs:flex-col xs:justify-between xs:p-0 lg:mt-0 lg:flex lg:flex-row lg:items-center lg:justify-end lg:gap-8 lg:p-0',
-        isOpen ? 'flex opacity-100' : 'xs:absolute xs:mt-20 xs:opacity-10 lg:static lg:opacity-100'
+        isOpen ? 'flex opacity-100' : 'xs:absolute xs:mt-96 xs:opacity-10 lg:static lg:opacity-100'
       )}
     >
       {children}
